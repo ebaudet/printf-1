925287962062f9099327bfea16156a60918bc551
simonboeuf <simon.boeuf@gmail.com>
2013-12-21 14:52:05 +0100

    Added X management

--- src/main.c
+++ src/main.c
@@ -15,10 +15,10 @@ int		main()
 	test = malloc(sizeof(int));
 	test2 = malloc(sizeof(int));
 	//printf("%S\n",1.2);
-	rslt = printf("%x%o\n%u\n%ls\n%CBon%-10cjour%s%%%nHello\n%-20s\n%- 50d\n",
-			n, n, n, L"Test Long string", (wint_t)'C', 'W', testnullstr, test, "CMON WTF NOW",42);
-	rslt2 = ft_printf("%x%o\n%u\n%ls\n%CBon%-10cjour%s%%%nHello\n%-20s\n%- 50i\n",
-			n, n, n, L"Test Long string", (wint_t)'C', 'W', testnullstr, test2, "CMON WTF NOW",42);
+	rslt = printf("%X\n%x\n%o\n%u\n%ls\n%CBon%-10cjour%s%%%nHello\n%-20s\n%- 50d\n",
+			1323532, n, n, n, L"Test Long string", (wint_t)'C', 'W', testnullstr, test, "CMON WTF NOW",42);
+	rslt2 = ft_printf("%X\n%x\n%o\n%u\n%ls\n%CBon%-10cjour%s%%%nHello\n%-20s\n%- 50i\n",
+			1323532, n, n, n, L"Test Long string", (wint_t)'C', 'W', testnullstr, test2, "CMON WTF NOW",42);
 	//rslt2 = ft_printf("Bonjour%%%-10sHello\n", "hi");
 
 	printf(" ---- \nprintf : %d\nft_printf : %d\n ----- \n", *test, *test2);
--- src/other.c
+++ src/other.c
@@ -144,12 +144,25 @@ int		printhexu(t_flag *flag, va_list args)
 {
 	unsigned int	nb;
 	int				rslt;
+	int				size;
 
-	nb = va_arg(args, int);
-	rslt = 0;
-	//TODO
-	flag = flag;
-	nb = nb;
+	nb = va_arg(args, unsigned int);
+	if (flag->plus)
+		ft_putchar('+');
+	if (!flag->plus && flag->space)
+		ft_putchar(' ');
+	if (flag->minus)
+	{
+		size = ft_putuhex(nb, 0, 0);
+		ft_printf_space(flag->width - size, flag->zero);
+	}
+	else
+	{
+		ft_printf_space(flag->width - size, flag->zero);
+		size = ft_putuhex(nb, 0, 0);
+	}
+	size += flag->space + flag->plus;
+	rslt = size < flag->width ? flag->width : size;
 	return (rslt);
 }
 
